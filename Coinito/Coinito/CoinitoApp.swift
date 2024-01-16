bacaa0f1de2c86352eb24bb78bfbb509e9d08bfd
MaToSens <matoosens@gmail.com>
2024-01-16 22:25:08 +0100

    Added initial files

---
+++ Coinito/Coinito/CoinitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinitoApp.swift
+//  Coinito
+//
+//  Created by MaTooSens on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
