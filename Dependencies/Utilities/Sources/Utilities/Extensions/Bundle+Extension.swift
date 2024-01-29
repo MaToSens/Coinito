c3caab920895fadac9f82ef8ef8a41c921fd0a79
MaToSens <matoosens@gmail.com>
2024-01-29 12:41:02 +0100

    Added Bundle extension with decodable and readFile methods

---
+++ Dependencies/Utilities/Sources/Utilities/Extensions/Bundle+Extension.swift
@@ -0,0 +1,39 @@
+//
+//  Bundle+Extension.swift
+//
+//
+//  Created by MaTooSens on 27/01/2024.
+//
+
+import Combine
+import Foundation
+
+public extension Bundle {
+    func decodable<T: Decodable>(
+        fileName: String,
+        withExtension fileExtension: String
+    ) -> AnyPublisher<T, Error> {
+        readFile(fileName: fileName, withExtension: fileExtension)
+            .decode(
+                type: T.self,
+                decoder: JSONDecoder(withKeyDecodingStategy: .convertFromSnakeCase)
+            )
+            .eraseToAnyPublisher()
+    }
+    
+    private func readFile(
+        fileName: String,
+        withExtension fileExtension: String
+    ) -> AnyPublisher<Data, Error> {
+        url(forResource: fileName, withExtension: fileExtension)
+            .publisher
+            .tryMap {
+                guard let data = try? Data(contentsOf: $0) else {
+                    throw CocoaError(.fileReadCorruptFile)
+                }
+                
+                return data
+            }
+            .eraseToAnyPublisher()
+    }
+}
